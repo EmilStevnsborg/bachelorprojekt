4fea099857325d38bf6e8d552fcfd8e845db6a31
EmilStevnsborg <emilstevnsborg.official@gmail.com>
2023-02-13 13:56:08 +0100

    small changes

--- conv_homemade.py
+++ conv_homemade.py
@@ -3,8 +3,11 @@ import numpy as np
 class Conv:
     # each filter contains a list of an array of kernels and a bias, where each kernel is dedicated to a single channel and the bias to the filter
     # kernels and channels will be numpy 2d arrays
-    def __init__(self, in_channels:int, filters):
+    # implement padding and stride
+    def __init__(self, in_channels: int, padding: int, stride: int, filters):
         self.in_channels = in_channels
+        self.padding = padding
+        self.stride = stride
         self.filters = filters
     
     def __call__(self, input):
@@ -22,7 +25,7 @@ class Conv:
             for i in range(self.in_channels):
                 channel2d = input[i]
                 kernel2d = kernels[i]
-                intermediates.append(self.execute(channel2d, kernel2d))
+                intermediates.append(self.kernel_operation(channel2d, kernel2d))
             
             out_channel = sum(intermediates) + bias
             outputs.append(out_channel)
@@ -31,12 +34,14 @@ class Conv:
 
 
     
-    def execute(self, channel2d, kernel2d):
+    def kernel_operation(self, channel2d, kernel2d):
         r, c = channel2d.shape
         kr, kc = kernel2d.shape
         out_channel = np.zeros((r-kr+1,c-kc+1))
+        # padding
         for i in range(r-kr+1):
             for j in range(c-kc+1):
+                # stride
                 channel2d_slice = channel2d[i:i+kr, j:j+kc]
                 ij = np.sum(np.multiply(kernel2d, channel2d_slice))
                 out_channel[i,j] = ij
--- conv_homemade_test.py
+++ conv_homemade_test.py
@@ -2,15 +2,15 @@ import conv_homemade
 import numpy as np
 
 # One channel 3x3
-input = [np.array([
+input1 = [np.array([
                     [1,2,3],
                     [4,5,6],
                     [7,8,9]
                             ])]
 
-# three filters one kernel 3x2 one bias for each kernel
+# three filters, one kernel 3x2 one bias for each filter
 filters1 = [
-            # one filter with one kernel (implying in_channel = 1) and bias
+            # In one filter one kernel (implying in_channel = 1)
             [[np.array([
                     [1,1],
                     [0,0],
@@ -27,25 +27,46 @@ filters1 = [
                     [1,1]
                             ])], 1]]
 
-# three filters one kernel 2x2 one bias for each kernel
+padding1 = 0
+stride1 = 0
+
+# Two channels 3x3
+input2 = [np.array([
+                    [1,2,3],
+                    [4,5,6],
+                    [7,8,9]
+                            ]),
+          np.array([
+                    [1,2,3],
+                    [4,5,6],
+                    [7,8,9]
+                            ])]
+
+# three filters, two kernels 2x2 one bias for each filter
 filters2 = [
-            # one filter with one kernel (implying in_channel = 1) and bias
+            # In one filter two kernels (implying in_channel = 2)
+            [[np.array([
+                    [2,2],
+                    [0,0]
+                            ]),
+              np.array([
+                     [0,0],
+                     [1,1]
+                             ])
+                                ], 1],
             [[np.array([
                     [1,1],
                     [0,0]
-                            ])], 1],
-            [[np.array([
-                    [0,0],
-                    [1,1]
-                            ])], 1],
-            [[np.array([
-                    [0,0],
-                    [0,0]
-                            ])], 1]]
+                            ]),
+              np.array([
+                     [0,0],
+                     [1,1]
+                             ])
+                                ], 0]]
 
 
-conv1_homemade = conv_homemade.Conv(in_channels=1, filters=filters2)
+conv1_homemade = conv_homemade.Conv(in_channels=1, padding=0, stride=0, filters=filters2)
 
-out = conv1_homemade(input=input)
+out = conv1_homemade(input=input1)
 
 print(out)
