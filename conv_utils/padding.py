04a48a9a164fce90f2f042b73a2c6c01b51a3bc2
unknown <sture.oksholm1@gmail.com>
2023-02-13 22:37:34 +0100

    Fixed some more comments

--- conv_utils/padding.py
+++ conv_utils/padding.py
@@ -9,11 +9,6 @@ def padding2d(input, kernel_size: tuple, pad_val: int, stride: tuple = None):
     if (stride == None):
         stride = kernel_size
 
-    # This should work if stride = kernel_size, however, it does not generalize 
-    #height_off = (kernel_size[0] * int(np.ceil(input_size[0]/kernel_size[0]))) % input_size[0]
-    #width_off = (kernel_size[1] * int(np.ceil(input_size[1]/kernel_size[1]))) % input_size[0]
-
-    # This should generalize, however, testing is needed
     height_off = input_size[0] - (kernel_size[0] + stride[0] * int(np.ceil(input_size[0]-kernel_size[0])/stride[0]))
     width_off = input_size[1] - (kernel_size[1] + stride[0] * int(np.ceil(input_size[1]-kernel_size[1])/stride[1]))
     
