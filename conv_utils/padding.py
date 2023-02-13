67326fbdef02b0fe510c5319395812e202b3ea66
unknown <sture.oksholm1@gmail.com>
2023-02-13 19:57:26 +0100

    Fixed stride problem - Testing is needed

--- conv_utils/padding.py
+++ conv_utils/padding.py
@@ -2,11 +2,21 @@ import numpy as np
 
 # CUSTOM STRIDE IS NOT IMPLEMENTED
 # IN THIS IMPLMENTATION THE KERNEL MUST BE SMALLER THAN THE INPUT
-def padding2d(input, kernel_size: tuple, pad_val: int):    
-    
+def padding2d(input, kernel_size: tuple, pad_val: int, stride: tuple = None):    
+
     input_size = input.shape
-    height_off = (kernel_size[0] * int(np.ceil(input_size[0]/kernel_size[0]))) % input_size[0]
-    width_off = (kernel_size[1] * int(np.ceil(input_size[1]/kernel_size[1]))) % input_size[0]
+
+    if (stride == None):
+        stride = kernel_size
+
+    # This should work if stride = kernel_size, however, it does not generalize 
+    #height_off = (kernel_size[0] * int(np.ceil(input_size[0]/kernel_size[0]))) % input_size[0]
+    #width_off = (kernel_size[1] * int(np.ceil(input_size[1]/kernel_size[1]))) % input_size[0]
+
+    # This should generalize, however, testing is needed
+    height_off = (kernel_size[0] + stride[0] * int(np.ceil(input_size[0]-kernel_size[0])/stride[0])) % input_size[0]
+    width_off = (kernel_size[1] + stride[0] * int(np.ceil(input_size[1]-kernel_size[1])/stride[1])) % input_size[1]
+    
     output_height = None
     output_width = None
 
