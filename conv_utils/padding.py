33c861f8defb5a844121da7b8fef42444ed9147f
unknown <sture.oksholm1@gmail.com>
2023-02-13 19:12:52 +0100

    Fixed bug

--- conv_utils/padding.py
+++ conv_utils/padding.py
@@ -1,22 +1,26 @@
 import numpy as np
 
 # CUSTOM STRIDE IS NOT IMPLEMENTED
-def padding2d(input, kernel_size: tuple, pad_val: int):
+# IN THIS IMPLMENTATION THE KERNEL MUST BE SMALLER THAN THE INPUT
+def padding2d(input, kernel_size: tuple, pad_val: int):    
     
     input_size = input.shape
-    height_off = input_size[0] % kernel_size[0]
-    width_off = input_size[1] % kernel_size[1]
+    height_off = (kernel_size[0] * int(np.ceil(input_size[0]/kernel_size[0]))) % input_size[0]
+    width_off = (kernel_size[1] * int(np.ceil(input_size[1]/kernel_size[1]))) % input_size[0]
     output_height = None
     output_width = None
+
+    assert input_size[0] >= kernel_size[0] and input_size[1] >= kernel_size[1]
 
     if (height_off == 0 and width_off == 0):
         return input
     if (height_off != 0):
-        output_height = input_size[0] % kernel_size[0] + input_size[0]
+        output_height = height_off + input_size[0]
     if (width_off != 0):
-        output_width = input_size[1] % kernel_size[1] + input_size[1]
+        output_width = width_off + input_size[1]
 
     output = np.full((output_height,output_width),pad_val)
+
     output[int(np.floor(height_off/2)):int(np.floor(height_off/2)) + input_size[0],
                     int(np.floor(width_off/2)):int(np.floor(width_off/2)) + input_size[1]] = input
     
