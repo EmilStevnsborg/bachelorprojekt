200eb9ff06ad2fbaaa8a1872d1e1c414cca19558
unknown <sture.oksholm1@gmail.com>
2023-04-16 14:52:40 +0200

    Redid the linear layer

--- Python/CNN_layers/linear_layer_homemade.py
+++ Python/CNN_layers/linear_layer_homemade.py
@@ -1,26 +1,38 @@
 import numpy as np
 import torch
 
+def flatten(input):
+    for i in range(len(input)):
+        if i == 0:
+            output = input[i]
+        else:
+            output = np.vstack((output,input[i]))
+    
+    return output.flatten()
 
 class linear_layer():
     def __init__(self,weights,bias,output_size: int):
         self.weights = weights.detach().numpy()
-        self.bias = bias
+        self.bias = bias.detach().numpy()
         self.output_size = output_size
 
-    def __call__(self, input):
+    def __call__(self, input: np.ndarray, flat: bool = True):
         input_size = np.array(input).shape
 
-        # Todo: For loop should also iterate over the batch_size
         for n in range(input_size[0]):
-            for i in range(int(self.output_size)):
-                if i == 0:
-                    tmp = torch.dot(input[n],torch.tensor(self.weights[i], dtype = torch.double)) + torch.tensor(self.bias[i], dtype = torch.double)
-                else:
-                    tmp = torch.hstack((tmp,(torch.dot(input[n],torch.tensor(self.weights[i], dtype = torch.double)) + torch.tensor(self.bias[i], dtype = torch.double))))
-            if n == 0:
-                output = tmp
+            if not(flat):
+                input_tmp = flatten(input[n])
             else:
-                output = torch.vstack((output,tmp))
+                input_tmp = input[n]
+            for i in range(int(self.output_size)):
+                tmp_weights = self.weights[i]
+                if i == 0:
+                    sample_output = np.matmul(np.array(input_tmp),tmp_weights) + self.bias[i]
+                else:
+                    sample_output = np.append(sample_output,np.matmul(np.array(input_tmp),tmp_weights) + self.bias[i])
+            if n == 0:
+                batch_output = sample_output
+            else:
+                batch_output = np.vstack((batch_output,sample_output))
     
-        return output
+        return batch_output
